6c4240aef2b11ef49b5b93a3b3d376f709777c05
AngelCreation <contact.angelcreations@gmail.com>
2016-09-15 20:46:17 +0530

    src, java and manifest uploades

---
+++ java/angel/com/sqlitedemo/Data.java
@@ -0,0 +1,36 @@
+package angel.com.sqlitedemo;
+
+/**
+ * Created by SVF 15213 on 02-07-2016.
+ */
+public class Data {
+    int id;
+    private String name;
+    private double salary;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
